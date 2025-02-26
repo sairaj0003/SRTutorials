660aa011898155b3b0c9087b7b98b53ced3288f5
Sai Raj Guduru <140234339+sairaj0003@users.noreply.github.com>
2025-02-26 16:18:52 +0530

    Update theme.js

--- static/js/theme.js
+++ static/js/theme.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (savedTheme) {
       body.setAttribute('data-theme', savedTheme);
-      themeIcon.src = savedTheme === 'dark' ? 'static/images/light_mode.png' : 'static/images/dark_mode.png';
+      themeIcon.src = savedTheme === 'dark' ? '../images/light_mode.png' : '../images/dark_mode.png';
       themeIcon.alt = savedTheme === 'dark' ? 'Dark Theme Icon' : 'Light Theme Icon';
   }
 });
@@ -17,12 +17,12 @@ function toggleTheme() {
 
   if (currentTheme === 'dark') {
       body.setAttribute('data-theme', 'light');
-      themeIcon.src = 'static/images/dark_mode.png';
+      themeIcon.src = '../images/dark_mode.png';
       themeIcon.alt = 'Light Theme Icon';
       localStorage.setItem('theme', 'light');  // Save to localStorage
   } else {
       body.setAttribute('data-theme', 'dark');
-      themeIcon.src = 'static/images/light_mode.png';
+      themeIcon.src = '../images/light_mode.png';
       themeIcon.alt = 'Dark Theme Icon';
       localStorage.setItem('theme', 'dark');  // Save to localStorage
   }
